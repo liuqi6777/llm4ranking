9fc1c989aaf928f0b6418039db01d433886c3122
liuqi6777 <qiliu6777@gmail.com>
2024-06-04 12:55:11 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='llm4ranking',
+    version='0.0.1',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    url='https://github.com/liuqi6777/llm4ranking',
+    license='MIT',
+    author='Qi Liu',
+    author_email='qiliu6777@gmail.com',
+    description='',
+    python_requires='>=3.8',
+    install_requires=[
+        'accelerate>=0.26.1',
+        'transformers>=4.37.2',
+    ]
+)
