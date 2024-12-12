6147a0ef73d083fb61fe3f5d23110d82eecee62c
liuqi6777 <qiliu6777@gmail.com>
2024-12-12 00:07:09 -0500

    feat: pairwise

--- src/llm4ranking/model/pairwise_generation.py
+++ src/llm4ranking/model/pairwise_generation.py
@@ -1,10 +1,17 @@
-import re
 from jinja2 import Template
 from typing import Optional
 
 from llm4ranking.model.lm import load_model
 
-PROMPT_TEMPLATE = ""
+
+PROMPT_TEMPLATE = """Given a query: {{ query }}, which of the following two documents is more relevant to the query?
+
+Document A: {{ doc1 }}
+
+Document B: {{ doc2 }}
+
+Only output "A" or "B", do not say anything else or explain.
+"""
 
 
 class PairwiseComparison:
@@ -18,28 +25,26 @@ class PairwiseComparison:
         self.lm = load_model(model_type, model_args)
         self.template = Template(prompt_template or PROMPT_TEMPLATE)
 
+    def __call__(self, query: str, doc1: str, doc2: str, **kwargs) -> int:
+        outputs = []
+        outputs.append(self.lm.generate(self.create_messages(query, doc1, doc2), **kwargs))
+        outputs.append(self.lm.generate(self.create_messages(query, doc2, doc1), **kwargs))
+        if self.parse_output(outputs[0]) == "a" and self.parse_output(outputs[1]) == "b":
+            score = 1
+        elif self.parse_output(outputs[0]) == "b" and self.parse_output(outputs[1]) == "a":
+            score = -1
+        else:
+            score = 0
+        return score
+
     def create_messages(
         self,
         query: str,
         doc1: str,
         doc2: str,
     ) -> str:
-        # TODO
-        input_context = f"Given a query: {query}, which of the following two passages is more relevant to the query?\n\nPassage A: {doc1}\n\nPassage B: {doc2}\n\nOutput Passage A or Passage B."
-        messages = [{"role": "user", "content": input_context}]
+        messages = [{"role": "user", "content": self.template.render(query=query, doc1=doc1, doc2=doc2)}]
         return messages
 
-    def __call__(self, query: str, doc1: str, doc2: str, **kwargs) -> int:
-        messages = self.create_messages(query, doc1, doc2) + self.create_messages(query, doc2, doc1)
-        outputs = self.lm.generate(
-            messages,
-            **kwargs,
-        )
-        # TODO: parse the output
-        if outputs[0] == "passage a" and outputs[1] == "passage b":
-            score = 1
-        elif outputs[0] == "passage a" and outputs[1] == "passage b":
-            score = -1
-        else:
-            score = 0
-        return score
+    def parse_output(self, output: str) -> str:
+        return output.strip().lower()[0]
