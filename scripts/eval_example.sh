a30f7fd1a47f00b23e599979bc76de5dca746d43
root <qiliu6777@gmail.com>
2026-05-20 07:19:00 +0000

    fix example script

--- scripts/eval_example.sh
+++ scripts/eval_example.sh
@@ -10,9 +10,9 @@ python -m llm4ranking.evaluation.evaluator \
   --model_type hf \
   --model_args model=$model \
   --reranking_approach rankgpt \
-  --reranking_args window_size=20,step=10,truncate_length=300 \
+  --strategy_args window_size=20,step=10,truncate_length=300 \
   --datasets dl19 \
-  --model_fw_args do_sample=False,max_new_tokens=128 \
+  --backend_args do_sample=False,max_new_tokens=128 \
   --topk 20
 
 
